4ebf81b325350344739ae913f00d1b3a898f0e74
Yuval Rimar <yuval.rimar@gmail.com>
2013-03-07 21:08:23 +0200

    object oriented lru rewrite

--- lru.lua
+++ lru.lua
@@ -19,89 +19,93 @@ function lru_cache_create(maxsize, evictfunc)
   -- They are sentinels which make the operations on the linked list easier
   lru_cache.head.prev = lru_cache.tail
   lru_cache.tail.next = lru_cache.head
-  return lru_cache
-end
 
-function lru_print(cache)
-  print("==== LRU ====")
-  local cur = cache.tail
-  while cur ~= nil do
-    print(cur.key, cur.value)
-    cur = cur.next
+  local function add_entry(cache, entry)
+    cache.head.prev.next = entry
+    entry.prev = cache.head.prev
+
+    cache.head.prev = entry
+    entry.next = cache.head
+
+    cache.current_items = cache.current_items + 1
+    cache.data[entry.key] = entry
   end
-end
 
-function lru_cache_add_entry(cache, entry)
-  cache.head.prev.next = entry
-  entry.prev = cache.head.prev
+  local function remove_entry(cache, entry)
+    cache.current_items = cache.current_items - 1
+    cache.data[entry.key] = nil
 
-  cache.head.prev = entry
-  entry.next = cache.head
-
-  cache.current_items = cache.current_items + 1
-  cache.data[entry.key] = entry
-end
-
-function lru_cache_remove_entry(cache, entry)
-  cache.current_items = cache.current_items - 1
-  cache.data[entry.key] = nil
-
-  entry.prev.next = entry.next
-  entry.next.prev = entry.prev
-end
-
-function lru_cache_del_last_entry(cache)
-  if cache.current_items ~= 0 then
-    local entry = cache.tail.next
-    lru_cache_remove_entry(cache, entry)
-
-    -- call callback function if it's present
-    if cache.evictfunc ~= nil then cache.evictfunc(entry.key, entry.value) end
+    entry.prev.next = entry.next
+    entry.next.prev = entry.prev
   end
-end
 
-function lru_cache_set(cache, key, value)
-  local entry = cache.data[key] 
-  if entry ~= nil then 
-    entry.value = value
-    lru_cache_remove_entry(cache, entry)
-    lru_cache_add_entry(cache, entry)
-  else
+  local function remove_last(cache)
+    if cache.current_items ~= 0 then
+      local entry = cache.tail.next
+      remove_entry(cache, entry)
 
-    local entry = {
-      -- If 'lru_cache_del_last_entry' is called the key is needed to remove
-      -- the entry from cache.data
-      key = key,
-      value = value,
-      next = nil,
-      prev = nil
-    }
-
-    -- print("current_items", cache.current_items)
-
-    if cache.current_items >= cache.maxsize then
-      lru_cache_del_last_entry(cache)
+      -- call callback function if it's present
+      if cache.evictfunc ~= nil then cache.evictfunc(entry.key, entry.value) end
     end
-    lru_cache_add_entry(cache, entry)
   end
-end
 
-function lru_cache_get(cache, key)
-  local entry = cache.data[key]
+  local mt = {}
+  mt.__index = {}
+  mt.__index.put = function(cache, key, value)
+    local entry = cache.data[key]
+    if entry ~= nil then
+      entry.value = value
+      remove_entry(cache, entry)
+      add_entry(cache, entry)
+    else
 
-  if entry == nil then return nil end
+      local entry = {
+        key = key,
+        value = value,
+        next = nil,
+        prev = nil
+      }
 
-  lru_cache_remove_entry(cache, entry)
-  lru_cache_add_entry(cache, entry)        
+      -- print("current_items", cache.current_items)
 
-  return entry.value
+      if cache.current_items >= cache.maxsize then
+        remove_last(cache)
+      end
+      add_entry(cache, entry)
+    end
+  end
+
+  mt.__index.get = function(cache, key)
+    local entry = cache.data[key]
+
+    if entry == nil then return nil end
+
+    remove_entry(cache, entry)
+    add_entry(cache, entry)
+
+    return entry.value
+  end
+
+  mt.__index.foreach = function(cache, func)
+    local cur = cache.tail.next
+    while cur ~= cache.head do
+      func(cur.key, cur.value)
+      cur = cur.next
+    end
+  end
+
+  return setmetatable(lru_cache, mt)
 end
 
 local cache = lru_cache_create(3, function(k, v) print("Evicted", k, v) end)
 
-lru_cache_set(cache, "a", "1")
-lru_cache_set(cache, "b", "2")
-lru_cache_set(cache, "a", "1")
-lru_cache_set(cache, "c", "3")
-lru_cache_set(cache, "d", "4")
-lru_print(cache)
+cache:put("a", "1")
+cache:put("b", "2")
+cache:put("a", "1")
+cache:put("c", "3")
+cache:put("d", "4")
+
+print("cache:get(d)", cache:get("d"))
+
+cache:foreach(print)
+
